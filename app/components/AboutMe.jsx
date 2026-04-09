788f54e9b2518c1b1bba91003629afe745400de7
im23a-braendlim <braendlim@bzz.ch>
2026-04-09 13:04:11 +0200

    updated aboutMe.jsx and made it more responsive

--- app/components/AboutMe.jsx
+++ app/components/AboutMe.jsx
@@ -5,12 +5,14 @@ export default function AboutMe() {
         <div id="aboutMe" className="bg-black sm:p-10 p-5 text-secondary">
             <h1 className="text-7xl uppercase font-druk sm:pt-12 pb-12 pt-20"><span
                 className="font-bebasneue uppercase text-[76px]">ü</span>ber mich</h1>
-            <div className="flex items-center flex-col-reverse lg:flex-row gap-4 justify-center ">
+            <div className="flex items-center flex-col-reverse lg:flex-row lg:gap-4 gap-10 justify-center ">
                 <div
-                    className="flex flex-col gap-7 lg:w-1/2 font-josefin w-full bg-secondary text-black border-primary border-2 p-10 rounded-xl">
+                    className="flex flex-col gap-7 lg:w-1/2 font-josefin w-full bg-secondary text-black border-primary border-2 sm:p-10 p-7 rounded-xl">
                     <p className="sm:text-lg text-md">
-                        Hallo, ich bin <span className="text-primary"> Michelle Brändli</span>, bin 18 Jahre alt und wohne in der Schweiz. Zurzeit bin ich eine
-                        Schülerin der 2.Klasse an der <span className="text-primary">IMS</span> in der <span className="text-primary">Kantonschule Hottingen</span>. In meiner Freizeit zeichne ich
+                        Hallo, ich bin <span className="text-primary"> Michelle Brändli</span>, bin 18 Jahre alt und
+                        wohne in der Schweiz. Zurzeit bin ich eine
+                        Schülerin der 2.Klasse an der <span className="text-primary">IMS</span> in der <span
+                        className="text-primary">Kantonschule Hottingen</span>. In meiner Freizeit zeichne ich
                         gerne, höre Musik und programmiere.
                     </p>
                     <Link href="https://github.com/Michcy"
@@ -20,13 +22,12 @@ export default function AboutMe() {
                         GitHub Profile
                     </Link>
                 </div>
-                <div className="flex lg:w-1/2 w-full justify-center items-center">
-                    <img src="./aboutMe.jpg" alt="image of myself and my drawings"/>
-                </div>
+                <img className="flex lg:w-1/2  w-full justify-center items-center" src="./aboutMe.jpg"
+                     alt="image of myself and my drawings"/>
             </div>
             <div className="pt-10 pb-10">
                 <img src="./Skizze.jpg" alt="Placeholder für eine Animation"
-                    className="w-full aspect-video object-cover"/>
+                     className="w-full aspect-video object-cover"/>
             </div>
         </div>
     )
